fad4c4fe0c8124c382c2db3ab48e68108de18b98
MatteoSanson <matteo.sanson@gmail.com>
2024-01-11 18:30:40 +0100

    feat: lettura dati

--- server.php
+++ server.php
@@ -2,6 +2,15 @@
 
 $database = file_get_contents(__DIR__ . '/dischi.json');
 
+
+// struttura php
 $dischi = json_decode($database);
 
-var_dump($dischi);
+// var_dump($dischi);
+
+
+
+// json con dischi
+header('Content-Type: application/json');
+echo json_encode($dischi);
+
--- src/js/index.js
+++ src/js/index.js
@@ -3,38 +3,18 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            dischi: [
-                {
-                    "album": "New Jersey",
-                    "author": "Bon Jovi",
-                    "released": "1988"
-                },
-                {
-                    "album": "Live at Wembley 86",
-                    "author": "Queen",
-                    "released": "1992"
-                },
-                {
-                    "album": "Ten's Summoner's Tales",
-                    "author": "Sting",
-                    "released": "1993"
-                },
-                {
-                    "album": "Steve Gadd Band",
-                    "author": "Steve Gadd Band",
-                    "released": "2018"
-                },
-                {
-                    "album": "Brave New World",
-                    "author": "Iron Maiden",
-                    "released": "2000"
-                },
-                {
-                    "album": "One more car, one more",
-                    "author": "Eric Clapton",
-                    "released": "2002"
-                }
-            ],
-        }
+            dischi: [],
+        };
+    },
+    methods: {
+        getDischi() {
+            axios.get('server.php').then((response) => {
+                console.log({ response });
+                this.dischi = response.data;
+            });
+        },
+    },
+    created() {
+        this.getDischi();
     }
 }).mount('#app');
